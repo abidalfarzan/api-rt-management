aa29a7cbe32f3695ceca8384f606ee101d4c134e
abidalfarzan <abidalfarzan02@gmail.com>
2026-06-01 07:50:41 +0700

    jenis iuran fix

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 
 return [
     'paths' => ['api/*'],
-    'allowed_origins' => ['http://localhost:5174'], // port default Vite React
+    'allowed_origins' => ['http://localhost:5173'], // port default Vite React
     'allowed_methods' => ['*'],
     'allowed_headers' => ['*'],
 ];
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\JenisIuranController;
 use App\Http\Controllers\Api\LaporanController;
 use App\Http\Controllers\Api\PembayaranIuranController;
 use App\Http\Controllers\Api\PengeluaranController;
 use App\Http\Controllers\Api\PenghuniController;
 use App\Http\Controllers\Api\RumahController;
-use Illuminate\Support\Facades\Route; // ✅ Ganti ini
+use Illuminate\Support\Facades\Route;
 
 Route::apiResource('houses', RumahController::class);
 Route::apiResource('residents', PenghuniController::class);
 Route::apiResource('payments', PembayaranIuranController::class);
 Route::apiResource('expenses', PengeluaranController::class);
 
+
 Route::post('houses/{house}/assign-resident', [RumahController::class, 'assignResident']);
 Route::post('houses/{house}/unassign-resident', [RumahController::class, 'unassignResident']);
+Route::get('fee-types', [JenisIuranController::class, 'index']);
 Route::get('reports/summary', [LaporanController::class, 'summary']);
 Route::get('reports/monthly/{year}/{month}', [LaporanController::class, 'monthly']);
